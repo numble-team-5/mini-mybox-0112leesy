d289e53019154ddd7f98d6e74d4348e9ceb3b42c
0112leesy <0112leesy@naver.com>
2023-03-15 01:48:08 +0900

    [B001]test:버킷 내 모든 오브젝트 조회 테스트

--- src/test/java/com/numble/mybox/S3Tests.java
+++ src/test/java/com/numble/mybox/S3Tests.java
@@ -8,6 +8,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +20,7 @@ public class S3Tests {
     final String regionName = "kr-standard";
     final String accessKey = "9Pzg3ZVTiF5OJQiAz6JI";
     final String secretKey = "tx3kg9Dwaam8RWClQh7EXmq9sSRJODOoSzVYJmXg";
+    final String bucketName = "mybox-test";
 
     // S3 client
     final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
@@ -38,6 +42,34 @@ public class S3Tests {
             e.printStackTrace();
         } catch (SdkClientException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getAllObjectTest() {
+        // list all in the bucket
+        try {
+            ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+                .withBucketName(bucketName)
+                .withMaxKeys(300);
+
+            ObjectListing objectListing = s3.listObjects(listObjectsRequest);
+
+            System.out.println("Object List:");
+            while (true) {
+                for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                    System.out.println("    name=" + objectSummary.getKey() + ", size=" + objectSummary.getSize() + ", owner=" + objectSummary.getOwner().getId());
+                }
+
+                if (objectListing.isTruncated()) {
+                    objectListing = s3.listNextBatchOfObjects(objectListing);
+                } else {
+                    break;
+                }
+            }
+        } catch (AmazonS3Exception e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
         }
     }
 
