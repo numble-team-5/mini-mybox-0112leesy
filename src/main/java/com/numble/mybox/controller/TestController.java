5740d8dd0732493241509b2058f0d692fd14e7af
0112leesy <0112leesy@naver.com>
2023-03-21 19:15:30 +0900

    [B002]feat:Spring Rest Docs 적용
    - TestController 테스트 및 api 명세 확인

--- src/main/java/com/numble/mybox/controller/TestController.java
+++ src/main/java/com/numble/mybox/controller/TestController.java
@@ -1,22 +1,29 @@
 package com.numble.mybox.controller;
 
+import com.numble.mybox.data.dto.RestDocsDto;
 import com.numble.mybox.data.entity.Test;
 import com.numble.mybox.service.TestService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class TestController {
-
     private final TestService testService;
-
     @GetMapping(value = "/api/test")
     public List<Test> getTestList() {
         return testService.getTestList();
     }
 
+    @PostMapping(value = "/api/test/rest_docs")
+    public ResponseEntity restDocs(@RequestBody RestDocsDto restDocsDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(restDocsDto);
+    }
 }
---
+++ src/main/java/com/numble/mybox/data/dto/RestDocsDto.java
@@ -0,0 +1,9 @@
+package com.numble.mybox.data.dto;
+
+import lombok.Data;
+
+@Data
+public class RestDocsDto {
+    String id;
+    String pw;
+}
